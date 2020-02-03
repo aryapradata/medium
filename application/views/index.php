c08d84d442178679f6b3c7cc659e6a622cb2be1d
cindy2400 <cdyy2400@gmail.com>
2020-02-03 10:45:11 +0700

    navbar

--- application/views/index.php
+++ application/views/index.php
@@ -8,37 +8,56 @@
     <link rel="stylesheet" href="css/header.css">
 
     <title>Document</title>
-</head>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+  </head>
 <body>
-    <header>
-    <div class="navbar">
-        <ul class="nav">
-          <li class="item">
-            <a href="#">Home</a>
-          </li>
+        
+<!--::header part start::-->
+<header class="main_menu home_menu">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-12">
+                    <nav class="navbar navbar-expand-lg navbar-light">
+                        <a class="navbar-brand" href="index.html"> <img src="img/m.png" alt="logo" height="60px" width="250px"> </a>
+                        <button class="navbar-toggler" type="button" data-toggle="collapse"
+                            data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                            aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
 
-          <li class="item">
-            <a href="#">Blog</a>
-          </li>
-
-          <li class="item">
-            <a href="#">About</a>
-          </li>
-
-          <li class="item">
-            <a href="#" class="contact">Contact</a>
-          </li>
-        </ul>
-
-        <div class="logo">
-          <img src="#">
+                        <div class="collapse navbar-collapse main-menu-item justify-content-end"
+                            id="navbarSupportedContent">
+                            <ul class="navbar-nav">
+                                <li class="nav-item active">
+                                    <a class="nav-link" href="index.html">Subscribe</a>
+                                </li>
+                                <li class="nav-item active">
+                                    <a class="nav-link" href="index.html">Write</a>
+                                </li>
+                                <li class="nav-item active">
+                                   
+                                </li>
+                                <ul class="nav justify-content-end">
+                                    <li class="nav-item">
+                                      <a class="nav-link active" data-toggle="modal" data-target="#exampleModal"> Sign In </a>
+                                    </li>
+                                </ul>
+                                <li class="nav-item active">
+                                    <button type="button" class="btn btn-success" data-toggle="modal" data-target="#exampleModal1">
+                                      Get Started
+                                    </button>  
+                                  </li>
+                            
+                                
+                            </ul>
+                        </div>
+                    </nav>
+                </div>
+            </div>
         </div>
-      </div>
     </header>
-
-    </header>
-
-    <script src="js/h_navbar.js"></script>
-
+    <!-- Header part end-->
+    
 </body>
 </html>
