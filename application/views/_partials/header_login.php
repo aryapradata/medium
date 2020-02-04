4d7c5b955561546a64d2aee969eeb17f667e54e2
cindy2400 <cdyy2400@gmail.com>
2020-02-04 16:57:59 +0700

    login edit

--- application/views/_partials/header_login.php
+++ application/views/_partials/header_login.php
@@ -4,19 +4,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-
-
-        
         
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
-    <link rel="stylesheet" href="css/header.css">
+    <link rel="stylesheet" href="<?= base_url('assets/css/style.css')?>" />
 
     <title>Medium - Get smarter about what matters to you.</title>
+
     <link rel="icon" href="<?= base_url('assets/img/logo.png')?>">
     <link rel="stylesheet" href="<?= base_url('assets/css/bootstrap.min.css')?>" />
 
     <header class="main_menu home_menu">
+
             <div class="container">
                 <div class="row align-items-center">
                     <div class="col-lg-12">
--- application/views/login.php
+++ application/views/login.php
@@ -1,5 +1,6 @@
 <?php $this->load->view("_partials/header_login.php"); ?>
 
+
 <hr>
 <div class="container">
   <div class="row">
@@ -10,12 +11,14 @@
             <img src="<?= base_url('assets/img/login.png')?>" height="100%" width="100%" > 
         </div>
         <div class="card-body">
-            <div class="boxberita">
-            <div class="berita">
-                <h5 class="card-title"><a href="" >Five Things You Don’t Want To Regret When You’re My Age</a></h5>
-                <p class="card-text"><a href="" >Nama Penulis</a></p>
-            </div>
-            </div>
+           
+                <div class="berita">
+                        <h5 class="card-title"><a href="" >Five Things You Don’t Want To Regret When You’re My Age</a></h5>
+                        <p class="card-text"><a href="" >Nama Penulis</a></p>
+                        
+                </div>
+                
+            
         </div>
         </div>
         <!-- end card -->
@@ -30,17 +33,38 @@
                 </div>
                 <div class="col-md-8">
                 <div class="card-body">
-                    <div class="boxberita1">
+                    
                     <div class="berita">
                         <h5 class="card-title"><a href="" >Five Things You Don’t Want To Regret When You’re My Age</a></h5>
                         <p class="card-text"><a href="" >Nama Penulis</a></p>
                     </div>
-                    </div>
+                    
                 </div>
                 </div>
             </div>
         </div>
         <!-- end card -->
+
+        <!-- card -->
+        <div class="card mb-3" style="max-width: 540px;">
+            <div class="row no-gutters">
+                <div class="col-md-4">
+                <img src="<?= base_url('assets/img/login.png')?>" height="100%" width="100%" >
+                </div>
+                <div class="col-md-8">
+                <div class="card-body">
+                   
+                    <div class="berita">
+                        <h5 class="card-title"><a href="" >Five Things You Don’t Want To Regret When You’re My Age</a></h5>
+                        <p class="card-text"><a href="" >Nama Penulis</a></p>
+                    </div>
+              
+                </div>
+                </div>
+            </div>
+        </div>
+        <!-- end card -->
+
 
 
     </div>
@@ -51,12 +75,12 @@
             <img src="<?= base_url('assets/img/login.png')?>" height="100%" width="100%" >
         </div>
         <div class="card-body">
-            <div class="boxberita">
+           
             <div class="berita">
                 <h5 class="card-title"><a href="" >Five Things You Don’t Want To Regret When You’re My Age</a></h5>
                 <p class="card-text"><a href="" >Nama Penulis</a></p>
             </div>
-            </div>
+            
         </div>
         </div>
         <!-- end card -->
@@ -68,20 +92,47 @@
 
 <div class="container">
 <div class="row">
-    <div class="col-8">
+    <div class="col-7">
 
     <!-- card -->
 
             <div class="card mb-3" style="max-width: 540px;">
         <div class="row no-gutters">
             <div class="col-md-4">
-            <img src="..." class="card-img" alt="...">
+                <img src="<?= base_url('assets/img/login.png')?>" height="100%" width="100%" >
             </div>
             <div class="col-md-8">
             <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+               
+                <div class="berita">
+                    <h5 class="card-title"><a href="" >Five Things You Don’t Want To Regret When You’re My Age</a></h5>
+                    <p class="card-text"><a href="" >Nama Penulis</a></p>
+                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                </div>
+              
+            </div>
+            </div>
+        </div>
+        </div>
+
+    <!-- end card -->
+
+    <!-- card -->
+
+    <div class="card mb-3" style="max-width: 540px;">
+        <div class="row no-gutters">
+            <div class="col-md-4">
+                <img src="<?= base_url('assets/img/login.png')?>" height="100%" width="100%" >
+            </div>
+            <div class="col-md-8">
+            <div class="card-body">
+               
+                <div class="berita">
+                    <h5 class="card-title"><a href="" >Five Things You Don’t Want To Regret When You’re My Age</a></h5>
+                    <p class="card-text"><a href="" >Nama Penulis</a></p>
+                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                </div>
+              
             </div>
             </div>
         </div>
@@ -90,17 +141,17 @@
     <!-- end card -->
 
     </div>
-    <div class="col-4">
+    <div class="col-5">
 
-        <!-- card -->
-        <div class="card" style="width: 18rem;">
-        <ul class="list-group list-group-flush">
-            <li class="list-group-item">Cras justo odio</li>
-            <li class="list-group-item">Dapibus ac facilisis in</li>
-            <li class="list-group-item">Vestibulum at eros</li>
-        </ul>
+        <div class="card w-30">
+        <div class="card-body">
+            <div class="berita">
+                <h5 class="card-title"><a href="" >Five Things You Don’t Want To Regret When You’re My Age</a></h5>
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <a href="#" class="btn btn-primary">Button</a>
+            </div>
         </div>
-        <!-- end card -->
+        </div>
 
     </div>
 </div>
