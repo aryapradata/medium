20fc21c440b1600a168f68541d876d5d33b94759
cindy2400 <cdyy2400@gmail.com>
2020-02-06 12:46:34 +0700

    create dan update story

--- application/views/stories/create_stories.php
+++ application/views/stories/create_stories.php
@@ -1,10 +1,43 @@
 <?php $this->load->view("_partials/header_login.php"); ?>
 
-    <form action="<?= base_url(); ?>stories/create" method="post">
-        Title: <input type="text" name="title"><br>
-        Content: <input type="text" name="content"><br>
-        Media: <input type="file" name="media"><br>
-        <button type="submit">Submit</button>
-    </form>
+<div class="csbg">
+	<div class="csbg2">
+		<center>
+			<div class="createstorybox">
+				<br>
+				<h7>Medium is truly where ideas </h7>
+				<br>
+				<h7> are born, shared, and spread.</h7>
+				<br>
+				<pre>All on Medium, all for you.</pre>
+				<hr>
+				<form action="<?= base_url(); ?>stories/create" method="post">
+					<table border="0">
+						<tr>
+							<td><b>Title</b></td>
+							<td>:</td>
+							<td><input type="text" name="title" size="40"></td>
+						</tr>
+						<tr>
+							<td><b>Content</b></td>
+							<td width="50px">:</td>
+							<div class="form-group">
+								<td><textarea class="form-control" name="content" rows="12"></textarea> </td>
+							</div>
+						</tr>
+						<tr>
+							<td><b>Media</b></td>
+							<td>:</td>
+							<td><input type="file" name="media" size="40"></td>
+						</tr>
+					</table>
+					<hr>
+					<button type="submit">Submit</button>
+					<hr>
+				</form>
+			</div>
+		</center>
+	</div>
+</div>
 
 <?php $this->load->view("_partials/footer_login.php"); ?>
