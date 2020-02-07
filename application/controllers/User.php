3463bdd9668154efccafed69fa3b6d90c0c3c13f
zorayaw <zorayaw31@gmail.com>
2020-02-08 06:42:57 +0700

    update added on username changes

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -1,14 +1,11 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
-class User extends CI_Controller
-{
-    public function __construct()
-    {
+class User extends CI_Controller {
+    public function __construct() {
         parent::__construct();
         $this->load->library('form_validation');
     }
-    public function index()
-    {
+    public function index() {
         if (!($this->session->userdata('email'))) {
             redirect('Auth');
         }
@@ -16,8 +13,7 @@ class User extends CI_Controller
         $this->load->view('user/user_profile', $data);
     }
 
-    public function get_data($id)
-    {
+    public function get_data($id) {
         if (!$this->session->userdata('email')) {
             redirect('user');
         }
@@ -25,14 +21,13 @@ class User extends CI_Controller
         $this->form_validation->set_rules('first_name', 'First_Name', 'required|trim');
         $this->form_validation->set_rules('last_name', 'Last_Name', 'trim');
         $this->form_validation->set_rules('username', 'Username', 'required|min_length[8]|is_unique[user.username]', [
-            'is_unique' => "This username has already registered!"
+            'is_unique' => "This username has already registered!",
         ]);
         $data['user'] = $this->db->get_where('user', ['user_id' => $id])->row_array();
         $this->load->view('user/edit_profile', $data);
     }
 
-    public function edit_profile($id)
-    {
+    public function edit_profile($id) {
         if (!$this->session->userdata('email')) {
             redirect('user');
         }
@@ -40,28 +35,42 @@ class User extends CI_Controller
         $last_name = $this->input->post('last_name');
         $username = $this->input->post('username');
         $bio = $this->input->post('bio');
-        $utemp = $this->db->get_where('user', ['user_id' => $id])->row_array();
+        $comtemp = $this->db->get_where('comment', ['username' => $this->session->userdata('username')])->row_array();
+        $claptemp = $this->db->get_where('clap', ['username' => $this->session->userdata('username')])->row_array();
+        $contemp = $this->db->get_where('content', ['username' => $this->session->userdata('username')])->row_array();
         $qtemp = array('username' => $username);
-        for ($i = 0; $i < count($utemp); $i++) {
-            $datatemp = $this->db->get_where('content', ['username' => $utemp['username']]);
-            $this->db->where('username', $utemp['username']);
+
+        for ($i = 0; $i < count($contemp); $i++) {
+            $this->db->get_where('content', ['username' => $this->session->userdata('username')]);
+            $this->db->where('username', $this->session->userdata('username'));
             $this->db->update('content', $qtemp);
         };
+        for ($i = 0; $i < count($claptemp); $i++) {
+            $this->db->get_where('clap', ['username' => $this->session->userdata('username')]);
+            $this->db->where('username', $this->session->userdata('username'));
+            $this->db->update('clap', $qtemp);
+        };
+        for ($i = 0; $i < count($comtemp); $i++) {
+            $this->db->get_where('comment', ['username' => $this->session->userdata('username')]);
+            $this->db->where('username', $this->session->userdata('username'));
+            $this->db->update('comment', $qtemp);
+}
+;
+
         $temp = array(
             'first_name' => $first_name,
             'last_name' => $last_name,
             'username' => $username,
-            'bio' => $bio
+            'bio' => $bio,
         );
         $this->db->where('user_id', $id);
         $this->db->update('user', $temp);
-
+        $this->session->set_userdata('username', $username);
         $data['user'] = $this->db->get_where('user', ['user_id' => $id])->row_array();
         redirect('user', $data);
     }
 
-    public function get_user($username)
-    {
+    public function get_user($username) {
         if (!$this->session->userdata('email')) {
             redirect('user');
         }
