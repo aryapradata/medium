6308f59905f2fc46b0f5fcd3bd301925709c7e48
IhtiarAlfath <tiaralfath@gmail.com>
2020-02-08 11:15:46 +0700

    draftt

--- application/views/stories/draft_stories.php
+++ application/views/stories/draft_stories.php
@@ -1,5 +1,7 @@
     <link rel="stylesheet" href="<?= base_url('assets/css/bootstrap/bootstrap.min.css')?>" />
     <link rel="stylesheet" href="<?= base_url('assets/css/style.css')?>" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+    	integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
     <?php $this->load->view("_partials/header_isi1.php"); ?>
     <?php $this->load->view("_partials/draft_header.php"); ?>
