f43d68b64725a3b178a0c2eb1a9e14e0af92b974
zorayaw <zorayaw31@gmail.com>
2020-02-09 11:32:20 +0700

    stories model repaired SOWWY GUYS

--- application/models/Stories_model.php
+++ application/models/Stories_model.php
@@ -1,41 +1,34 @@
 <?php
 
-
-class Stories_model extends CI_Model
-{
-    public function getAllStories()
-    {
+class Stories_model extends CI_Model {
+    public function getAllStories() {
         return $this->db->get('content')->result_array();
     }
-  
-    public function getStoriesById($id)
-    {
+
+    public function getStoriesById($id) {
         $this->db->select('content.*, user.first_name, user.last_name');
         $this->db->from('user');
         $this->db->join('content', 'content.username = user.username');
-        $this->db->where('content_id',$id);
+        $this->db->where('status_stories', 1);
+        $this->db->where('content_id', $id);
         return $this->db->get()->result_array();
     }
 
-    public function getStoriesByMyUsernameStatus0()
-    {
-        $this->db->where('username',$this->session->userdata('username'));
-        $this->db->where('status_stories',0);
+    public function getStoriesByMyUsernameStatus0() {
+        $this->db->where('username', $this->session->userdata('username'));
+        $this->db->where('status_stories', 0);
         return $this->db->get('content')->result_array();
     }
 
-    public function getStoriesByMyUsernameStatus1()
-    {
-        $this->db->where('username',$this->session->userdata('username'));
-        $this->db->where('status_stories',1);
+    public function getStoriesByMyUsernameStatus1() {
+        $this->db->where('username', $this->session->userdata('username'));
+        $this->db->where('status_stories', 1);
         return $this->db->get('content')->result_array();
     }
 
-    public function getStoriesByStatus($search = null)
-    {
-        if($search)
-        {
-            $this->db->like('title',$search);
+    public function getStoriesByStatus($search = null) {
+        if ($search) {
+            $this->db->like('title', $search);
         }
         $this->db->select('content.*, user.first_name, user.last_name');
         $this->db->from('user');
@@ -44,68 +37,59 @@ class Stories_model extends CI_Model
         return $this->db->get()->result_array();
     }
 
-    public function getCommentByContentId($id)
-    {
-        $data = [
-            'username' => $this->session->userdata('username'),
-            'title' => $this->input->post('title'),
-            'content' => $this->input->post('content'),
-            'media' => $this->input->post('media'),
-            'status_stories' => 0,
-            'clap' => 0
-        ];
-
-        $this->db->insert('content', $data);
+    public function getCommentByContentId($id) {
+        $this->db->select('comment.*, user.first_name, user.last_name');
+        $this->db->from('user');
+        $this->db->join('comment', 'comment.username = user.username');
+        $this->db->where('content_id', $id);
+        return $this->db->get()->result_array();
     }
 
-    
-    public function insertImage(){
-        $config['upload_path']='./images/';
-        $config['allowed_types']='jpg|png|jpeg';
-        $config['max_size']='3000';
-        $config['remove_space']= TRUE;
+    public function insertImage() {
+        $config['upload_path'] = './images/';
+        $config['allowed_types'] = 'jpg|png|jpeg';
+        $config['max_size'] = '3000';
+        $config['remove_space'] = true;
 
-        $this->load->library('upload',$config);
-        
-        if($this->upload->do_upload('media')){
-            $return = ['result'=>'success',
-                        'file' => $this->upload->data(),
-                        'error'=>''];
-        }else{
-            $return=['result'=>'failed',
-                    'file'=>'',
-                    'error'=>$this->upload->display_errors()];
+        $this->load->library('upload', $config);
+
+        if ($this->upload->do_upload('media')) {
+            $return = ['result' => 'success',
+                'file' => $this->upload->data(),
+                'error' => ''];
+        } else {
+            $return = ['result' => 'failed',
+                'file' => '',
+                'error' => $this->upload->display_errors()];
         }
-        
+
         return $return;
     }
 
-    public function createStories($upload)
-    {
+    public function createStories($upload) {
         $data = [
             'username' => $this->session->userdata('username'),
             'title' => $this->input->post('title'),
             'content' => $this->input->post('content'),
-            'media'=> $upload['file']['file_name'],
+            'media' => $upload['file']['file_name'],
             'status_stories' => 0,
-            'clap' => 0
+            'clap' => 0,
         ];
 
         $this->db->insert('content', $data);
     }
 
-    public function createStoriesMediaNull()
-    {
-        $data = [
-            'username' => $this->session->userdata('username'),
-            'title' => $this->input->post('title'),
-            'content' => $this->input->post('content'),
-            'media' => "No Media",
-            'status_stories' => 0,
-            'clap' => 0
-        ];
-        $this->db->insert('content', $data);
-    }
+    // public function createStoriesMediaNull() {
+    //     $data = [
+    //         'username' => $this->session->userdata('username'),
+    //         'title' => $this->input->post('title'),
+    //         'content' => $this->input->post('content'),
+    //         'media' => "No Media",
+    //         'status_stories' => 0,
+    //         'clap' => 0,
+    //     ];
+    //     $this->db->insert('content', $data);
+    // }
 
     // public function createStoriesTitleNull()
     // {
@@ -133,33 +117,32 @@ class Stories_model extends CI_Model
     //     $this->db->insert('content',$data);
     // }
 
-    public function updateImage(){
-        $config['upload_path']='./images/';
-        $config['allowed_types']='jpg|png|jpeg';
-        $config['max_size']='3000';
-        $config['remove_space']= TRUE;
+    public function updateImage() {
+        $config['upload_path'] = './images/';
+        $config['allowed_types'] = 'jpg|png|jpeg';
+        $config['max_size'] = '3000';
+        $config['remove_space'] = true;
 
-        $this->load->library('upload',$config);
-        
-        if($this->upload->do_upload('media')){
-            $return = ['result'=>'success',
-                        'file' => $this->upload->data(),
-                        'error'=>''];
-        }else{
-            $return=['result'=>'failed',
-                    'file'=>'',
-                    'error'=>$this->upload->display_errors()];
+        $this->load->library('upload', $config);
+
+        if ($this->upload->do_upload('media')) {
+            $return = ['result' => 'success',
+                'file' => $this->upload->data(),
+                'error' => ''];
+        } else {
+            $return = ['result' => 'failed',
+                'file' => '',
+                'error' => $this->upload->display_errors()];
         }
-        
+
         return $return;
     }
 
-    public function updateStories($id,$upload)
-    {
+    public function updateStories($id, $upload) {
         $data = [
             'title' => $this->input->post('title'),
             'content' => $this->input->post('content'),
-            'media'=> $upload['file']['file_name']
+            'media' => $upload['file']['file_name'],
         ];
 
         $this->db->where('content_id', $id);
@@ -190,60 +173,50 @@ class Stories_model extends CI_Model
     //     $this->db->update('content',$data);
     // }
 
-    public function deleteStories($id)
-    {
-        $this->db->where('content_id',$id);
+    public function deleteStories($id) {
+        $this->db->where('content_id', $id);
         $this->db->delete('content');
-        $this->db->where('content_id',$id);
+        $this->db->where('content_id', $id);
         $this->db->delete('comment');
     }
 
-    public function publish($id)
-    {
-        $this->db->where('content_id',$id);
-        $this->db->update('content',['status_stories' => 1]);
+    public function publish($id) {
+        $this->db->where('content_id', $id);
+        $this->db->update('content', ['status_stories' => 1]);
     }
 
-    public function addComment($id)
-    {
+    public function addComment($id) {
         $data = [
             'username' => $this->session->userdata('username'),
             'content_id' => $id,
-            'comment' => $this->input->post('comment')
+            'comment' => $this->input->post('comment'),
         ];
-        $this->db->insert('comment',$data);
+        $this->db->insert('comment', $data);
     }
 
-    public function deleteComment($contentid,$commentid)
-    {
-        $this->db->where('comment_id',$commentid);
+    public function deleteComment($contentid, $commentid) {
+        $this->db->where('comment_id', $commentid);
         $this->db->delete('comment');
     }
 
-    public function updateComment($contentid,$commentid)
-    {
+    public function updateComment($contentid, $commentid) {
         $data = [
-            'comment' => $this->input->post('comment')
+            'comment' => $this->input->post('comment'),
         ];
 
-        $this->db->where('comment_id',$commentid);
-        $this->db->update('comment',$data);
+        $this->db->where('comment_id', $commentid);
+        $this->db->update('comment', $data);
     }
 
-
-    public function insertCelap($data){
+    public function insertCelap($data) {
         return $this->db->insert('celap', $data);
     }
- 
-    public function updateCelap($data){
-        return $this->db->update('celap', $data); 
+
+    public function updateCelap($data) {
+        return $this->db->update('celap', $data);
     }
 
-    public function getClap()
-    {
+    public function getClap() {
         return $this->db->get('celap')->result_array();
     }
 }
-
-        
-
--- application/views/stories/open_stories.php
+++ application/views/stories/open_stories.php
@@ -1,70 +1,70 @@
 
-    <link rel="stylesheet" href="<?= base_url('assets/css/bootstrap/bootstrap.min.css')?>" />
-    <link rel="stylesheet" href="<?= base_url('assets/css/style.css')?>" />
+    <link rel="stylesheet" href="<?=base_url('assets/css/bootstrap/bootstrap.min.css')?>" />
+    <link rel="stylesheet" href="<?=base_url('assets/css/style.css')?>" />
 
-    <?php $this->load->view("_partials/header_isi1.php"); ?>
+    <?php $this->load->view("_partials/header_isi1.php");?>
     </head>
 
     <body>
-    	<?php foreach ($stories as $val) : ?>
+    	<?php foreach ($stories as $val): ?>
     	<div class="container content">
     		<div class="row">
     			<div class="col-sm-2">
     			</div>
     			<div class="col-7">
     				<h1>
-    					<?= $val['title']; ?>
+    					<?=$val['title'];?>
     				</h1>
-    				<a href=""> <img src="<?= base_url('assets/img/comment/comment_1.png')?>" height="45px" width="45px"
+    				<a href=""> <img src="<?=base_url('assets/img/comment/comment_1.png')?>" height="45px" width="45px"
     						align="left"> </a>
     				<p class="content-size">
-    					<?= $val['first_name']; ?> <?= $val['last_name']; ?>
+    					<?=$val['first_name'];?> <?=$val['last_name'];?>
     				</p>
     			</div>
     			<div class="col">
     			</div>
     		</div>
 
-    		<a href=""><img src="<?= base_url().'images/'.$val['media']?>" alt=""></a>
+    		<a href=""><img src="<?=base_url() . 'images/' . $val['media']?>" alt=""></a>
     		<br>
     		<div class="container content content-size">
     			<div class="row">
     				<div class="col-sm-2">
     					<p></p>
     				</div>
-    				<div class="col-7">>
+    				<div class="col-7">
     					<p>
-    						<?= $val['content']; ?>
+    						<?=$val['content'];?>
     					</p>
     				</div>
     				<div class="col ">
 
     				</div>
 				</div>
-				<form action="<?= base_url(); ?>stories/createComment/<?= $val['content_id']; ?>" method="post">
+				<form action="<?=base_url();?>stories/createComment/<?=$val['content_id'];?>" method="post">
                   <input type="text" name="comment" placeholder="Comment...">
                   <button type="submit">Submit</button>
-              </form>  
+              </form>
     		</div>
     	</div>
 
-    	<?php endforeach; ?>
+    	<?php endforeach;?>
 
 
-    	<?php foreach ($comment as $val) : ?>
+    	<?php foreach ($comment as $val): ?>
     	<table border="1">
     		<tr>
     			<td>
-    				<b><?= $val['first_name']; ?> <?= $val['last_name']; ?></b>
-    				<p><?= $val['comment']; ?></p>
+    				<b><?=$val['first_name'];?> <?=$val['last_name'];?></b>
+    				<p><?=$val['comment'];?></p>
     				<a
-    					href="<?= base_url(); ?>stories/deleteComment/<?= $val['content_id']; ?>/<?= $val['comment_id']; ?>">Delete</a>
+    					href="<?=base_url();?>stories/deleteComment/<?=$val['content_id'];?>/<?=$val['comment_id'];?>">Delete</a>
     			</td>
     		</tr>
     	</table>
-    	<?php endforeach; ?>
+    	<?php endforeach;?>
 
 
-    	<?php $this->load->view("_partials/footer_login.php"); ?>
+    	<?php $this->load->view("_partials/footer_login.php");?>
 
 
